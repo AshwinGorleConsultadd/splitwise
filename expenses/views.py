048650f0cf2d0202a348b3f9fe475a52f58faf72
Ashwin Gorle <ashwin.g@consultadd.com>
2025-07-18 16:14:01 +0530

    fix: transaction error while creating expense

--- expenses/views.py
+++ expenses/views.py
@@ -1,7 +1,7 @@
 
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .utils import validate_expense_request
+from .utils import validate_expense_request, split_expense
 from django.contrib.auth.models import User
 from .models import Expense, ExpenseShare, ExpensePayer
 from groups.models import Group
@@ -13,18 +13,23 @@ def expenses_view(request):
     if request.method == 'POST':
         return create_expense(request)
     elif request.method == 'GET':
-        return get_expenses(request)
+        return None
     else:
         return JsonResponse({"error": "Method not allowed"}, status=405)
 
 def create_expense(request):
-    expense = json.loads(request.body)
+    # expense = json.loads(request.body)
+    expense = Expense.objects.get(id=7)
+    print("expense : ",expense)
+    split_expense(expense)
+    return JsonResponse({"message" : "expense created successfully"},status=201)
     #validate request body
     validation_result = validate_expense_request(expense)
     if validation_result != 'pass':
         return JsonResponse({'message' : validation_result})
 
     #create expense
+    print('creating new expense')
     gorup = Group.objects.get(id=expense['group'])
     new_expense = Expense.objects.create(
         title=expense['title'],
@@ -33,6 +38,7 @@ def create_expense(request):
     )
 
     #create expense shares
+    print('creating new expense_share')
     for x in expense['participants']:
         user_id = x['user_id']
         share_amount = x['share_amount']
@@ -44,6 +50,7 @@ def create_expense(request):
         )
     
     #create expense payers
+    print('creating new expense_payer')
     for x in expense['payers']:
         user_id = x['user_id']
         paid_amount = x['paid_amount']
@@ -54,7 +61,7 @@ def create_expense(request):
             paid_amount = paid_amount
         )
 
-
+    
 
     print('request body:', expense)
     return JsonResponse({"message" : "expense created successfully"},status=201)
--- my_django_project/settings.py
+++ my_django_project/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'authapi',
     'expenses',
     'settlements'
-    
 ]
 
 MIDDLEWARE = [
